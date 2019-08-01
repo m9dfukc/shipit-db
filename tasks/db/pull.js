e7270ea372204798d0f85ba9ca6a8b57043bfa92
m9dfukc <andreas@invertednothing.com>
2019-08-01 16:13:07 +0200

    added a yes/no prompt for safety reasons

--- tasks/db/pull.js
+++ tasks/db/pull.js
@@ -1,9 +1,10 @@
 var utils = require('shipit-utils');
 var path = require('path');
 var db = require('../../lib/db');
+var prompt = require('../../lib/yesno');
 
 module.exports = function(gruntOrShipit) {
-  var task = function task() {
+  var task = async function task() {
     var shipit = db(utils.getShipit(gruntOrShipit));
     var dumpFile = shipit.db.dumpFile('remote');
     var remoteDumpFilePath = path.join(shipit.sharedPath || shipit.currentPath, dumpFile);
@@ -13,20 +14,27 @@ module.exports = function(gruntOrShipit) {
       return shipit.copyFromRemote(remoteDumpFilePath, localDumpFilePath);
     };
 
-    return shipit.db.createDirs()
-    .then(function() {
-      return shipit.db.dump('remote', remoteDumpFilePath);
-    })
-    .then(download)
-    .then(function() {
-      return shipit.db.clean('remote', remoteDumpFilePath, shipit.config.db.cleanRemote);
-    })
-    .then(function() {
-      return shipit.db.load('local', localDumpFilePath);
-    })
-    .then(function() {
-      return shipit.db.clean('local', localDumpFilePath, shipit.config.db.cleanLocal);
+    var ok = await prompt({
+      question: 'This task is destuctive, there is no rollback as of now!\nAre you sure you want to pull remote db into local? (y/n)'
     });
+    if (ok) {
+      return shipit.db.createDirs()
+      .then(function() {
+        return shipit.db.dump('remote', remoteDumpFilePath);
+      })
+      .then(download)
+      .then(function() {
+        return shipit.db.clean('remote', remoteDumpFilePath, shipit.config.db.cleanRemote);
+      })
+      .then(function() {
+        return shipit.db.load('local', localDumpFilePath);
+      })
+      .then(function() {
+        return shipit.db.clean('local', localDumpFilePath, shipit.config.db.cleanLocal);
+      });
+    } else {
+      shipit.log('... Skip db:pull task!');
+    }
   };
 
   utils.registerTask(gruntOrShipit, 'db:pull:task', task);
--- tasks/db/push.js
+++ tasks/db/push.js
@@ -1,9 +1,10 @@
 var utils = require('shipit-utils');
 var path = require('path');
 var db = require('../../lib/db');
+var prompt = require('../../lib/yesno');
 
 module.exports = function(gruntOrShipit) {
-  var task = function task() {
+  var task = async function task() {
     var shipit = db(utils.getShipit(gruntOrShipit));
     var remoteDumpFilePath = path.join(shipit.sharedPath || shipit.currentPath, shipit.db.dumpFile('local'));
     var localDumpFilePath = path.join(shipit.config.workspace, shipit.db.dumpFile('local'));
@@ -11,20 +12,27 @@ module.exports = function(gruntOrShipit) {
       return shipit.remoteCopy(localDumpFilePath, remoteDumpFilePath);
     };
 
-    return shipit.db.createDirs()
-    .then(function() {
-      return shipit.db.dump('local', localDumpFilePath);
-    })
-    .then(upload)
-    .then(function() {
-      return shipit.db.clean('local', localDumpFilePath, shipit.config.db.cleanLocal);
-    })
-    .then(function() {
-      return shipit.db.load('remote', remoteDumpFilePath);
-    })
-    .then(function() {
-      return shipit.db.clean('remote', remoteDumpFilePath, shipit.config.db.cleanRemote);
+    var ok = await prompt({
+      question: 'This task is destuctive, there is no rollback as of now!\nAre you sure you want to push local db into remote? (y/n)'
     });
+    if (ok) {
+      return shipit.db.createDirs()
+      .then(function() {
+        return shipit.db.dump('local', localDumpFilePath);
+      })
+      .then(upload)
+      .then(function() {
+        return shipit.db.clean('local', localDumpFilePath, shipit.config.db.cleanLocal);
+      })
+      .then(function() {
+        return shipit.db.load('remote', remoteDumpFilePath);
+      })
+      .then(function() {
+        return shipit.db.clean('remote', remoteDumpFilePath, shipit.config.db.cleanRemote);
+      });
+    } else {
+      shipit.log('... Skip db:push task!');
+    }
   };
 
   utils.registerTask(gruntOrShipit, 'db:push:task', task);
